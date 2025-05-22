6e1f9ff2cd5275e04a801654bc9f78eeaa2e6e96
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-05-22 23:58:18 +0300

    Favorites view and viewModel added.

--- BlindID_Movie/Tabbar/Tabbar.swift
+++ BlindID_Movie/Tabbar/Tabbar.swift
@@ -29,7 +29,7 @@ struct MainTabView: View {
                     Text("Home")
                 }
 
-            LoginView()
+            FavoritesView()
                 .tabItem {
                     Image("heartIcon")
                         .renderingMode(.template)
