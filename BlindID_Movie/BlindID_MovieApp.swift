6730180a166a3bb10b9b1bd37906173f13a14c98
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-05-22 08:51:26 +0300

    Initial Commit

---
+++ BlindID_Movie/BlindID_MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlindID_MovieApp.swift
+//  BlindID_Movie
+//
+//  Created by Samet Korkmaz on 22.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlindID_MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlindID_Movie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlindID_Movie
+//
+//  Created by Samet Korkmaz on 22.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
