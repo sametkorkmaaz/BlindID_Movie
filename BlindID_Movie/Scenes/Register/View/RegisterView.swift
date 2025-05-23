025dac3753ac18c5c6e395b4c5f5a0356904d1a1
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-05-24 00:59:22 +0300

    RegisterView error popup added.

--- BlindID_Movie/Scenes/Register/View/RegisterView.swift
+++ BlindID_Movie/Scenes/Register/View/RegisterView.swift
@@ -11,80 +11,103 @@ struct RegisterView: View {
     @StateObject private var viewModel = RegisterViewModel()
     @AppStorage("isLoggedIn") private var isLoggedIn: Bool = false
     @State private var isPasswordVisible = false
+    @State private var showErrorPopup = false
 
     var body: some View {
-        VStack(spacing: 24) {
-            Image(viewModel.constants.blindImageName)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 240, height: 120)
-                .padding(.top, 48)
+        ZStack {
+            VStack(spacing: 24) {
+                Image(viewModel.constants.blindImageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 240, height: 120)
+                    .padding(.top, 48)
 
-            VStack(spacing: 8) {
-                Text(viewModel.constants.welcomeText)
-                    .font(FontManager.poppinsBold(size: 28))
+                VStack(spacing: 8) {
+                    Text(viewModel.constants.welcomeText)
+                        .font(FontManager.poppinsBold(size: 28))
 
-                Text(viewModel.constants.registerMessageText)
-                    .foregroundColor(.grayG30)
-                    .font(FontManager.poppinsRegular(size: 14))
-            }
+                    Text(viewModel.constants.registerMessageText)
+                        .foregroundColor(.grayG30)
+                        .font(FontManager.poppinsRegular(size: 14))
+                }
 
-            Group {
-                TextField(viewModel.constants.namePlaceholder, text: $viewModel.name)
-                TextField(viewModel.constants.surnamePlaceholder, text: $viewModel.surname)
-                TextField(viewModel.constants.emailPlaceholder, text: $viewModel.email)
-                    .autocapitalization(.none)
+                Group {
+                    TextField(viewModel.constants.namePlaceholder, text: $viewModel.name)
+                    TextField(viewModel.constants.surnamePlaceholder, text: $viewModel.surname)
+                    TextField(viewModel.constants.emailPlaceholder, text: $viewModel.email)
+                        .autocapitalization(.none)
 
-                HStack {
-                    if isPasswordVisible {
-                        TextField(viewModel.constants.passwordPlaceholder, text: $viewModel.password)
-                    } else {
-                        SecureField(viewModel.constants.passwordPlaceholder, text: $viewModel.password)
-                    }
-                    Button(action: {
-                        isPasswordVisible.toggle()
-                    }) {
-                        Image(systemName: isPasswordVisible ? viewModel.constants.passwordEyeSlashIcon : viewModel.constants.passwordEyeIcon)
-                            .foregroundColor(.gray)
+                    HStack {
+                        if isPasswordVisible {
+                            TextField(viewModel.constants.passwordPlaceholder, text: $viewModel.password)
+                        } else {
+                            SecureField(viewModel.constants.passwordPlaceholder, text: $viewModel.password)
+                        }
+                        Button(action: {
+                            isPasswordVisible.toggle()
+                        }) {
+                            Image(systemName: isPasswordVisible ? viewModel.constants.passwordEyeSlashIcon : viewModel.constants.passwordEyeIcon)
+                                .foregroundColor(.gray)
+                        }
                     }
                 }
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(12)
+
+                Button {
+                    Task {
+                        await viewModel.register()
+                        if viewModel.isSuccess {
+                            isLoggedIn = true
+                        } else if viewModel.errorMessage != nil {
+                            showErrorPopup = true
+                        }
+                    }
+                } label: {
+                    Text(viewModel.constants.registerButtonText)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(30)
+                }
+                .disabled(viewModel.isLoading)
+
+                if let success = viewModel.successMessage {
+                    Text(success)
+                        .foregroundColor(.green)
+                        .font(FontManager.poppinsMedium(size: 12))
+                }
+
+                Spacer()
             }
             .padding()
-            .background(Color(.systemGray6))
-            .cornerRadius(12)
 
-            Button {
-                Task {
-                    await viewModel.register()
-                    if viewModel.isSuccess {
-                        isLoggedIn = true
-                    }
+            if showErrorPopup {
+                PopupView(isPresented: $showErrorPopup)
+                    .transition(.opacity)
+                    .zIndex(2)
+            }
+
+            if viewModel.isLoading {
+                ZStack {
+                    Color.black.opacity(0.3)
+                        .ignoresSafeArea()
+
+                    BlindAnimationView()
+                        .padding()
+                        .background(Color.darkBG)
+                        .cornerRadius(16)
+                        .shadow(radius: 10)
+                        .frame(width: 200, height: 150)
                 }
-            } label: {
-                Text(viewModel.constants.registerButtonText)
-                    .foregroundColor(.white)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .cornerRadius(30)
+                .transition(.opacity)
+                .zIndex(1)
             }
-            .disabled(viewModel.isLoading)
-
-            if let success = viewModel.successMessage {
-                Text(success)
-                    .foregroundColor(.green)
-                    .font(FontManager.poppinsMedium(size: 12))
-            } else if let error = viewModel.errorMessage {
-                Text(error)
-                    .foregroundColor(.red)
-                    .font(FontManager.poppinsMedium(size: 12))
-            }
-            Spacer()
         }
-        .padding()
     }
 }
-
 
 #Preview {
     RegisterView()
